a3268d0547b2433f3635536bfd2e594d07d68d52
VeryOriginalIk <voros.bence@students.jedlik.eu>
2023-01-25 09:41:23 +0100

    jatek

--- jatek.py
+++ jatek.py
@@ -3,11 +3,21 @@ import sys
 import time
 from mentes import *
 questek = {}
-inventory = ()
+inventory = {}
 hp = 100
+stamina = 100
+morale = 100
 def Jatek():
     try:
         MentesOlvas()
     except:
         name = input("Hogy hívnak? ")
-        print(f"Szia, {name}!\n")
+        print(f"Épp kinyitod a WC ajtót, amikor egy szék repül el az arcod előtt.\nMegígérted a feleségednek, hogy ma nem verekszel, de a barátaid hívnak.\nMit teszel?")
+        choice1 = input("1 - Beavatkozol\t2 - Kérsz egy sört\t3 - Kimész")
+        if choice1 == 1:
+            KocsmaVerekedés ()
+        elif choice1 == 2:
+            stamina -= 30
+            morale -= 10
+        elif choice1 == 3:
+            Utca()
