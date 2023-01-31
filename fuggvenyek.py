5f65c7254fca39378328f50d4c58ed217716ea46
Donát <zsoldos.donat.dominik@students.jedlik.eu>
2023-01-31 10:11:22 +0100

    asd

--- fuggvenyek.py
+++ fuggvenyek.py
@@ -237,6 +237,54 @@ def menu():
 
 #def harc():
     #Hajrá Donát
+from fuggvenyek import *
+
+class Character:
+    def __init__(self, name, hp, atk, morale):
+        self.name = name
+        self.hp = hp
+        self.atk = atk
+        self.MR = morale
+
+    def get_stats(self):
+        stats = {"Name": self.name,
+                 "HP": self.hp,
+                 "ATK": self.atk,
+                 "Moral": self.MR
+                 }
+        return stats
+
+
+
+def battle(player, enemy):
+    while player.hp > 0 and enemy.hp > 0:
+        print("Harc lehetőségek:")
+        print("1. Támadás")
+        print("2. Gyógyítás")
+
+        choice = input("")
+
+        if choice == "1":
+            enemy.hp -= player.atk
+            print(f"{name}", player.atk, f"Sebzéz {enemy}-ra/re.")
+            fegyverDurability -= 1
+            if fegyverDurability == 0:
+                fegyver = 0
+        elif choice == "2":
+            player.hp += 30
+            energiaital -= 1
+        else:
+            print("nincs ilyen lehetőség.")
+
+        if enemy.hp > 0:
+            player.hp -= enemy.atk
+            print(f"{enemy}", enemy.atk, f"sebzett {name}-ra/re.")
+
+    if player.hp > 0:
+        print(f"{name} megnyerte a csatát")
+    else:
+        print(f"Meghaltál :c")
+
 
 #sztori kezdete
 def Kocsma():
@@ -355,3 +403,9 @@ def KocsmaVerekedes():
     szoveg = "Úgy döntesz, segítesz barátaidnak, de előtte szükséged lesz egy fegyverre.\n"
     irdki(szoveg)
     KocsmaItem()
+    player = Character(name, hp, attack, morale)
+    # enemyn = 
+    # enemyhp =
+    # enemyat = 
+    enemy = Character(enemyn, enemyhp, enemyat, 0)
+    battle(player, enemy)
