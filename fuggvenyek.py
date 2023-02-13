a899b659000fb44de40a4e64f2889cbaf50e4589
Vörös Bence <100135396+VeryOriginalIK@users.noreply.github.com>
2023-02-13 19:15:26 +0100

    kiiras frissites

--- fuggvenyek.py
+++ fuggvenyek.py
@@ -9,8 +9,7 @@ def mentes():
     f = open("mentes.txt", "w", encoding="utf-8")
     f.write(f"{vars(jatekos)}")
     f.close()
-    irdki("A mentés sikeres. :D", 3)
-    menu()
+    irdki(f"A mentés sikeres. :D\n", "",3)
 
 def betoltes():
     try:
@@ -20,15 +19,19 @@ def betoltes():
                 values = [v for k, v in d.items()]
                 global jatekos
                 jatekos = Jatekos(values[0] ,values[1] ,values[2] ,values[3], values[4] ,values[5] ,values[6] ,values[7] ,values[8] ,values[9] ,values[10])
-                Kocsma()
-        else:
-            irdki("Még nincs mentésed!", 3)
-            menu()
         f.close()
+        irdki(f"Mentés betöltve. Üdv a játékban, {jatekos.name}!\n" ,"", 4)
     except:
-        pass
+        irdki("Még nincs mentésed!","", 3)
+        menu()
 
-def irdki(szoveg, sleep):
+def irdki(szoveg, karakter, sleep):
+    if karakter == "segito":
+        szoveg = f"\033[92m{szoveg}\033[0m"
+    if karakter == "" or karakter == "narrator":
+        szoveg = f"\033[1m {szoveg} \033[0m"
+    #if karakter == "enemy":
+    #    szoveg = idk
     for i in range(len(szoveg)):
         print(szoveg[i], end='', flush=True); 
         time.sleep(0.01)
@@ -37,7 +40,7 @@ def irdki(szoveg, sleep):
 #Először hogy mik közül választhat, majd listába a választható számokat.
 #Így nem kell minden lehetőségnél újra beírni
 def beker(szoveg, options):
-    irdki(szoveg, 0)
+    irdki(szoveg ,"", 0)
     option = 0
     while option not in options:
         option = input("")
@@ -51,11 +54,11 @@ def beker(szoveg, options):
         except:
             pass
         if option not in options:
-            irdki(f"Nincs ilyen lehetőség!\n", 0)
+            irdki(f"Nincs ilyen lehetőség!\n", "", 0)
     return option
 
 def kilep():
-    irdki("Köszönjük, hogy játszottál.", 3)
+    irdki("Köszönjük, hogy játszottál.","", 3)
     sys.exit()
     os.system('cls')
     
@@ -63,7 +66,7 @@ def menu():
     os.system('cls')
     option = beker(f"1 - Új játék\t2 - Mentés betöltése\t3 - Kilépés\n",[1, 2, 3])
     if option == 1:
-        kar_keszites()    
+        kar_keszites()   
         Kocsma()
     elif option == 2:
         betoltes()
@@ -77,12 +80,13 @@ def vege():
 #harcrendszer
 
 def kar_keszites():
-    irdki("Hogy hívnak?\n- ", 0)
+    irdki("Hogy hívnak?\n-", "", 0)
     name = sys.stdin.readline().strip()
     global jatekos
     jatekos = Jatekos({} ,0 ,0 ,"" ,0 ,5 ,100 ,100 ,100 ,0 ,name)
-    irdki(f"Üdv a játékban, {jatekos.name}. Ajánljuk, hogy a játék elején növeld a képernyőd méretét a neked megfelelőre.\n", 2.5)
-    irdki("Nyugi, megvárunk.", 2.5)
+    irdki(f"Üdv a játékban, {jatekos.name}!\n","", 2.5)
+    irdki(f'Bármikor nyomhatsz "m"-et, ha menteni akarsz, vagy "k"-t a kilépéshez.\n',"", 3)
+    irdki("Jó játékot!\n" ,"", 4)
     os.system('cls')
 
 
@@ -90,24 +94,24 @@ def tamadas():
     attack = round((jatekos.stamina+jatekos.fegyver+jatekos.morale)/(random.randrange(1 , 100))*10)
     enemy.hp -= attack
     if attack < 30:
-        irdki("Hát ez lecsúszott. ", 0)
+        irdki("Hát ez lecsúszott. ","", 0)
     elif 300 > attack > 100:
-        irdki("Na most jól betaláltál. ", 0)
+        irdki("Na most jól betaláltál. ","", 0)
     elif attack > 300:
-        irdki("Hát te aztán nem aprózod el! ", 0)
+        irdki("Hát te aztán nem aprózod el! ","", 0)
     irdki(f"{attack} életponttal sebezted {enemy.name}-t.", 0)
     jatekos.fegyverDurability -= 1
     if jatekos.fegyverDurability == 0:
-        irdki(f"\nSajnos a harc a {jatekos.fegyverNev}edet sem kímélte. Széttört {enemy.name} fején.", 0.1)
+        irdki(f"\nSajnos a harc a {jatekos.fegyverNev}edet sem kímélte. Széttört {enemy.name} fején.", "", 0.1)
         jatekos.fegyver = 0
         jatekos.fegyverNev = ""
 
 
 def battle():
     while jatekos.hp > 0 and enemy.hp > 0:
-        irdki(f"Te: {jatekos.hp}\n", 0.1)
-        irdki(f"{enemy.name}: {enemy.hp}\n\n", 0.1)
-        irdki(f"{jatekos.energiaital} energiaitalod van.\n", 0.2)
+        irdki(f"Te: {jatekos.hp}\n","", 0.1)
+        irdki(f"{enemy.name}: {enemy.hp}\n\n","", 0.1)
+        irdki(f"{jatekos.energiaital} energiaitalod van.\n", "",0.2)
         option = beker(f"Harc lehetőségek:\n1. Támadás\t\t2. Gyógyítás\n",[1, 2])
         if option == 1:
             tamadas()
@@ -117,25 +121,25 @@ def battle():
                 jatekos.energiaital -= 1
                 irdki("Ittál egy Spar Energy Drinket, a HP-d 100-al nő. ", 0)
             else:
-                irdki(f"Kifogytál a piából.\n", 0)
+                irdki(f"Kifogytál a piából.\n","", 0)
                 tamadas()
 
         if enemy.hp > 0:
             enemyattack = round(random.randrange(5, 60))
             jatekos.hp -= enemyattack
-            irdki(f"\n{enemy.name} {enemyattack} -at sebzett rád.\n", 0)
+            irdki(f"\n{enemy.name} {enemyattack} -at sebzett rád.\n","", 0)
 
     if jatekos.hp > 0:
-        irdki(f" {enemy.name}-nek vége van.\n", 0)
+        irdki(f" {enemy.name}-nek vége van.\n","", 0)
     else:
-        irdki(f"\nMeghaltál :c", 3)
+        irdki(f"\nMeghaltál :c","", 3)
         vege()
 
 #sztori kezdete
 def Kocsma():
-    print(f"Szeva {jatekos.name}")
-    print(f"                                ____                                     \n                   _           |---||            _                         \n                   ||__________|SSt||___________||                                \n                  /_ _ _ _ _ _ |:._|'_ _ _ _ _ _ _\`.                      \n                 /_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\:`.                   \n                /_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\::`.                 \t\t\t\t ____        _____           __                      __ \n               /:.___________________________________\:::`-._             \t\t\t\t/_   |     _/ ____\____     |__| ____ ________ _____/  |_  /\ \n           _.-'_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _`::::::`-.._        \t\t\t\t |   |     \   __\/ __ \    |  |/ __ \\___   // __ \   __\  \/ \n\t_.-' _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ `:::::::::`-._    \t\t\t\t |   |      |  | \  ___/    |  \  ___/ /    /\  ___/|  |   /\  \n\t,'_:._________________________________________________`:_.::::-';`   \t\t\t\t |___| /\   |__|  \___>  \__|  |\____  \\_____ \____ |  |   \/       \n      `.'/ || |:::::`.'/::::::::`.'/::::::::`.'/::::::|.`.'/.|     :|     \t\t\t\t       \/             \/\______|    \/       \/     |____                 \n       ||  || |::::::||::::::::::||::::::::::||:::::::|..||..|     ||     \t\t\t\t   _____           \n       ||  || |  __  || ::  ___  || ::  __   || ::    |..||;||     ||     \t\t\t\t  /  _  \     |  | ______   ____   ______ _____ _____     \n       ||  || | |::| || :: |:::| || :: |::|  || ::    |.|||:||_____||__   \t\t\t\t  /  _  \     |  | ______   ____   ______ _____ _____ \n       ||  || | |::| || :: |:::| || :: |::|  || ::    |.|||:||_|_|_||,(   \t\t\t\t /  /_\  \    |  |/ /  _ \_/ ___\ /  ___//     \\__  \  \n       ||_.|| | |::| || :: |:::| || :: |::|  || ::    |.'||..|    _||,|   \t\t\t\t/    |    \   |    <  <_> )  \___ \___ \|  Y Y  \/ __\_ \n    .-'::_.:'.:-.--.-::--.-:.--:-::--.--.--.-::--.--.-:.-::,'.--.'_|| |   \t\t\t\t\____|__  /   |__|_ \____/ \_____>_____>|_| |__ (____ /\n     );||_|__||_|__|_||__|_||::|_||__|__|__|_||__|__|_|;-'|__|_(,' || '-  \t\t\t\t        \/         \/          \/                   \/\n     ||||  || |. . . ||. . . . . ||. . . . . ||. . . .|::||;''||   ||:'   \n     ||||.;  _|._._._||._._._._._||._._._._._||._._._.|:'||,, ||,,           \n     '''''           ''-         ''-         ''-         '''  '''         ", 3)
-    irdki(f"\nÉpp kinyitod a WC ajtót, amikor egy szék repül el az arcod előtt.\nMegígérted otthon, hogy ma nem verekszel, de a barátaidnak segítség kell.\nMit teszel?", 0)
+    print(f"                                ____                                     \n                   _           |---||            _                         \n                   ||__________|SSt||___________||                                \n                  /_ _ _ _ _ _ |:._|'_ _ _ _ _ _ _\`.                      \n                 /_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\:`.                   \n                /_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _\::`.                 \t\t\t\t ____        _____           __                      __ \n               /:.___________________________________\:::`-._             \t\t\t\t/_   |     _/ ____\____     |__| ____ ________ _____/  |_  /\ \n           _.-'_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _`::::::`-.._        \t\t\t\t |   |     \   __\/ __ \    |  |/ __ \\___   // __ \   __\  \/ \n\t_.-' _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ `:::::::::`-._    \t\t\t\t |   |      |  | \  ___/    |  \  ___/ /    /\  ___/|  |   /\  \n\t,'_:._________________________________________________`:_.::::-';`   \t\t\t\t |___| /\   |__|  \___>  \__|  |\____  \\_____ \____ |  |   \/       \n      `.'/ || |:::::`.'/::::::::`.'/::::::::`.'/::::::|.`.'/.|     :|     \t\t\t\t       \/             \/\______|    \/       \/     |____                 \n       ||  || |::::::||::::::::::||::::::::::||:::::::|..||..|     ||     \t\t\t\t   _____           \n       ||  || |  __  || ::  ___  || ::  __   || ::    |..||;||     ||     \t\t\t\t  /  _  \     |  | ______   ____   ______ _____ _____     \n       ||  || | |::| || :: |:::| || :: |::|  || ::    |.|||:||_____||__   \t\t\t\t  /  _  \     |  | ______   ____   ______ _____ _____ \n       ||  || | |::| || :: |:::| || :: |::|  || ::    |.|||:||_|_|_||,(   \t\t\t\t /  /_\  \    |  |/ /  _ \_/ ___\ /  ___//     \\__  \  \n       ||_.|| | |::| || :: |:::| || :: |::|  || ::    |.'||..|    _||,|   \t\t\t\t/    |    \   |    <  <_> )  \___ \___ \|  Y Y  \/ __\_ \n    .-'::_.:'.:-.--.-::--.-:.--:-::--.--.--.-::--.--.-:.-::,'.--.'_|| |   \t\t\t\t\____|__  /   |__|_ \____/ \_____>_____>|_| |__ (____ /\n     );||_|__||_|__|_||__|_||::|_||__|__|__|_||__|__|_|;-'|__|_(,' || '-  \t\t\t\t        \/         \/          \/                   \/\n     ||||  || |. . . ||. . . . . ||. . . . . ||. . . .|::||;''||   ||:'   \n     ||||.;  _|._._._||._._._._._||._._._._._||._._._.|:'||,, ||,,           \n     '''''           ''-         ''-         ''-         '''  '''         ",)
+    time.sleep(3)
+    irdki(f"\nÉpp kinyitod a WC ajtót, amikor egy szék repül el az arcod előtt.\nMegígérted otthon, hogy ma nem verekszel, de a barátaidnak segítség kell.\nMit teszel?","", 2)
     Choice1()
 
 def Choice1():
@@ -147,57 +151,58 @@ def Choice1():
             jatekos.stamina -= 20
             jatekos.morale -= 10
             jatekos.sörszam += 1
-            irdki(f"Már {jatekos.sörszam} sört ittál.", 0)
+            irdki(f"Már {jatekos.sörszam} sört ittál.","", 0)
             Choice1()
         else:
-            irdki(f"Ez már a 10. sör volt. Forog veled a szoba, majd minden elsötétül.", 4)
+            irdki(f"Ez már a 10. sör volt. Forog veled a szoba, majd minden elsötétül.","", 4)
             os.system('cls')
             for i in range(0,3):
                 print('.', end="")
                 time.sleep(2)
-            irdki(f"\nEgy árokban ébredsz. Nem kellett volna annyit innod. Zúg a fejed. ", 3)
-            irdki("Lassan hazadülöngélsz, már nincs kedved sehova menni.", 3)
+            irdki(f"\nEgy árokban ébredsz. Nem kellett volna annyit innod. Zúg a fejed. ", "",3)
+            irdki("Lassan hazadülöngélsz, már nincs kedved sehova menni.", "",3)
             vege()
     elif option == 3:
         ElsoQuest()
 
 def ElsoQuest():
-    irdki(f"Indulni készülsz. Az ajtóban valaki megállít.\n", 2)
-    irdki(f'\033[1m' + "-Ha segítesz nekem ígérem megjutalmazlak, de sietnünk kell!-\n" + '\033[0m', 2)
-    irdki(f"Suttogja az idegen miközben ide-oda nézeget, mintha keresne valakit.", 2)
+    irdki(f"Indulni készülsz. Az ajtóban valaki megállít.\n", "",2)
+    irdki(f"-Ha segítesz nekem ígérem megjutalmazlak, de sietnünk kell!\n","segito", 2)
+    irdki(f"Suttogja az idegen miközben ide-oda nézeget, mintha keresne valakit.","", 2)
     option = beker(f"\nMit teszel?\n1 - Elmész az idegenennel\t2 - Félrelököd és kimész\t3 - Visszamész a kocsmába\n",[1, 2, 3])
     if option == 1:
         Piac()
     elif option == 2:
-            irdki(f"A falnak csapódik, de egy kést ránt elő a mellénye zsebéből és a hasadba döfi!\n", 2)
+            irdki(f"A falnak csapódik, de egy kést ránt elő a mellénye zsebéből és a hasadba döfi!\n", "",2)
             vege()
     elif option == 3:
-        irdki(f"Mindenki a földön vonaglik és jajgatózik. Elrémít a látvány, úgy döntesz mégis az idegennel mész.\n", 0)
+        irdki(f"Mindenki a földön vonaglik és jajgatózik. Elrémít a látvány, úgy döntesz mégis az idegennel mész.\n","", 0)
         jatekos.morale -= 30
         Piac()
 
 def Piac():
-    irdki(f"Követed az idegent a piacra.\n", 1)
-    irdki(f'\n\033[1m' + '-Ahová mész ott szükséged lesz egy fegyverre!-' + '\n\033[0m', 2)
-    irdki(f"Odamegy a kovácshoz és egy fényes pengével tér vissza.\n-Tedd el, az első ingyen van!-\n", 2)
+    irdki(f"Követed az idegent a piacra.\n","", 1)
+    irdki(f'-Ahová mész ott szükséged lesz egy fegyverre!-\n', "segito",2)
+    irdki(f"Odamegy a kovácshoz és egy fényes pengével tér vissza.\n-","",1)
+    irdki(f"Tedd el, az első ingyen van!-\n","segito", 2)
     option = beker(f"Mit teszel?\n1 - Elteszed a kardot\t\t2 - Elutasítod az ajándékot\n",[1, 2])
     if option == 1:
         jatekos.fegyver = 200
         jatekos.fegyverDurability = 20
         jatekos.fegyverNev = "vajazó kés"
-        irdki(f"Elveszed a kardot az idegentől.", 1)
+        irdki(f"Elveszed a kardot az idegentől.", "",1)
         # szoveg = f"-Jólvan, kövesd az utat az erdőig. A fák között találsz majd egy ösvényt, az elvezet a helyre ahova menned kell.\nHa találsz ott egy medált kérlek hozd vissza nekem!"
-        irdki(f'\n\033[1m' + '-Jólvan, kövesd az utat az erdőig. A fák között találsz majd egy ösvényt, az elvezet a helyre ahova menned kell.\nHa találsz ott egy medált kérlek hozd vissza nekem!-' + '\033[0m', 4)
+        irdki(f'-Jólvan, kövesd az utat az erdőig. A fák között találsz majd egy ösvényt, az elvezet a helyre ahova menned kell.\nHa találsz ott egy medált kérlek hozd vissza nekem!-',"segito", 4)
         # irdki(f"-Jólvan, kövesd az utat az erdőig. A fák között találsz majd egy ösvényt, az elvezet a helyre ahova menned kell.\nHa találsz ott egy medált kérlek hozd vissza nekem!")
         TutorialBoss()
     elif option == 2:
-        irdki(f'\n\033[1m' + "-Csalódtam benned! Ha ilyen gyáva vagy, akkor menj el!-" + '\n\033[0m', 4)
+        irdki(f"-Csalódtam benned! Ha ilyen gyáva vagy, akkor menj el!-", "segito", 4)
         vege()
 
 def TutorialBoss():
-    irdki(f"\nAz eredei út végen egy kopár mezőt találsz. Úgy tűnik senki sincs a közelben, de olyan érzésed van mintha figyelnének.\n", 2)
-    irdki(f"Elindulsz a láda felé amit a pusztaság közepén láttál meg. Mikor már majdnem odaértél rálépsz egy csontra, mely hangos reccsenéssel törik szét a lábad alatt!\n", 4)
-    irdki(f"Hangos üvöltést hallasz a fák közül, a hang az egész testedben félelmet kelt. Már nincs időd elfutni mert egy szörny ront rád, harcolnod kell az életedért!\n", 0)
+    irdki(f"\nAz eredei út végen egy kopár mezőt találsz. Úgy tűnik senki sincs a közelben, de olyan érzésed van mintha figyelnének.\n", "segito", 2)
+    irdki(f"Elindulsz a láda felé amit a pusztaság közepén láttál meg. Mikor már majdnem odaértél rálépsz egy csontra, mely hangos reccsenéssel törik szét a lábad alatt!\n","", 4)
+    irdki(f"Hangos üvöltést hallasz a fák közül, a hang az egész testedben félelmet kelt. Már nincs időd elfutni mert egy szörny ront rád, harcolnod kell az életedért!\n", "",0)
     global enemy
     enemy = Enemy("", 400, 20)
     battle()
@@ -213,35 +218,35 @@ def KocsmaItem():
         jatekos.fegyver = 40
         jatekos.fegyverDurability = 8
         jatekos.fegyverNev = "sörösüveg"
-        irdki("A pulthoz csapod az üveged végét, és kész is a bökő. ", 0)
+        irdki("A pulthoz csapod az üveged végét, és kész is a bökő. ","", 0)
     elif option == 3:
         jatekos.fegyverDurability = -1
-        irdki("Kemény legény vagy, pár suhancért nem kell fegyverhez nyúlnod. Puszta kézzel szállsz velük harcba. ", 0)
+        irdki("Kemény legény vagy, pár suhancért nem kell fegyverhez nyúlnod. Puszta kézzel szállsz velük harcba. ", "",0)
     elif option == 4:
         Choice1()
         
 def KocsmaVerekedes():
-    irdki("Úgy döntesz, segítesz barátaidnak, de előtte szükséged lesz egy fegyverre.\n", 0)
+    irdki("Úgy döntesz, segítesz barátaidnak, de előtte szükséged lesz egy fegyverre.\n","", 0)
     KocsmaItem()
     global enemy
     enemy = Enemy("Győző",200, 20)
-    irdki(f"{enemy.name} részegesen feléd ballag.\n", 0)
+    irdki(f"{enemy.name} részegesen feléd ballag.\n","", 0)
     battle()
     jatekos.energiaital += 3
     jatekos.penz += 800
     irdki("Győzőéket elvertétek, de már nincs kedved ünnepelni. Elveszed kiterült ellenfeledtől 3 energiaitalát, meg találsz nála pár 100ast. Buszjegyre jó lesz.\n", 0)
     ElsoQuest()
-    irdki(f"Indulni készülsz. Az ajtóban valaki megállít.\n", 2)
-    irdki(f"-Ha segítesz nekem ígérem megjutalmazlak, de sietnűnk kell!-\n", 2)
-    irdki(f"Suttogja az idegen miközben ide-oda nézeget, mintha keresne valakit.", 2)
+    irdki(f"Indulni készülsz. Az ajtóban valaki megállít.\n","", 2)
+    irdki(f"-Ha segítesz nekem ígérem megjutalmazlak, de sietnűnk kell!-\n", "segito",2)
+    irdki(f"Suttogja az idegen miközben ide-oda nézeget, mintha keresne valakit.","", 2)
     option = beker(f"\nMit teszel?\n1 - Elmész az idegenennel\t2 - Félrelököd és kimész\t3 - Visszamész a kocsmába\n",[1, 2, 3])
     if option == 1:
         ElsoQuest()
     elif option == 2:
-        irdki(f"A falnak csapódik, de egy kést ránt elő a mellénye zsebéből és a hasadba döfi!\n", 2)
+        irdki(f"A falnak csapódik, de egy kést ránt elő a mellénye zsebéből és a hasadba döfi!\n","", 2)
         vege()
     elif option == 3:
-        irdki(f"Mindenki a földön vonaglik és jajgatózik. Elrémít a látvány, úgy döntesz mégis az idegennel mész.\n", 1.5)
+        irdki(f"Mindenki a földön vonaglik és jajgatózik. Elrémít a látvány, úgy döntesz mégis az idegennel mész.\n","", 1.5)
         jatekos.morale -= 30
         ElsoQuest()
 
